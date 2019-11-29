c48ef654c674de64798b3a4f8d56efe5a8e1b02d
Rutile3 <Rutile3@outlook.jp>
2019-11-29 11:06:32 +0900

    [ファイル追加] 日記を表示するリダイレクトページとそのスクリプトを追加

---
+++ Scrapbox/RutileDiary/ShowToday/ShowToday.js
@@ -0,0 +1,12 @@
+
+(function () {
+  
+  const project_url = "RutileDiary";
+
+  const date = new Date();
+  const yy = (""  +  date.getFullYear()  ).slice(-2);
+  const mm = ("0" + (date.getMonth() + 1)).slice(-2);
+  const dd = ("0" +  date.getDate()      ).slice(-2);
+
+  location.href = "https://scrapbox.io/" + project_url + "/" + yy + mm + dd;
+})();
